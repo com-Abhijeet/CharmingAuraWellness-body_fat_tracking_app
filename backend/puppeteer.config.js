a3198061cbce152175e818065d4588e461064112
com-Abhijeet <shindeabhijeet552@gmail.coom>
2025-02-09 23:14:17 +0530

    updated backend configs3

--- backend/puppeteer.config.js
+++ backend/puppeteer.config.js
@@ -1,4 +1,8 @@
-import { join } from "path";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const puppeteerConfig = {
   cacheDirectory: join(__dirname, ".cache", "puppeteer"),
